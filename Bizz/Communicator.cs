bfbd2aea94e7e796a54bbcbb615969bfd77f806f
bern0145 <gywerd@gmail.com>
2018-02-04 13:44:33 +0100

    Communicator

--- Bizz/Communicator.cs
+++ Bizz/Communicator.cs
@@ -2,11 +2,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Net;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
 using System.Diagnostics;
 
 namespace Bizz
@@ -26,19 +26,32 @@ namespace Bizz
             return content;
         }
 
+        private void CleanFolder()
+        {
+            DirectoryInfo di = new DirectoryInfo(@"C:\%userprofile%\Downloads\");
+            foreach (FileInfo file in di.GetFiles())
+            {
+                if (file.Name == "mini_installer.exe")
+                {
+                    file.Delete();
+                }
+            }
+        }  
+
 
         private void DownloadBuild()
         {
+            CleanFolder();
             string latestVer = CheckVer();
             string downloadURL = "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Win_x64/" + latestVer + "/mini_installer.exe";
             WebClient Client = new WebClient();
-            Client.DownloadFile(downloadURL, @"C:\TEMP\Chromium\mini_installer.exe");
+            Client.DownloadFile(downloadURL, @"C:\%userprofile%\Downloads\mini_installer.exe");
         }
 
         public void UpdateBuild()
         {
             DownloadBuild();
-            Process.Start(@"C:\TEMP\Chromium\mini_installer.exe");
-        } 
+            Process.Start(@"C:\%userprofile%\Downloads\mini_installer.exe");
+        }
     }
  }
