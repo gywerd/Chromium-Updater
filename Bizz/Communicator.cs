1fff0f280ea14d15e01db52750b1095d7bbbdc26
bern0145 <gywerd@gmail.com>
2018-02-04 04:49:27 +0100

    Communicator class

---
+++ Bizz/Communicator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace Bizz
+{
+    public class Communicator
+    {
+        public Communicator() { }
+
+
+
+        private string CheckVer()
+        {
+            WebClient client = new WebClient();
+            Stream stream = client.OpenRead("https://storage.googleapis.com/chromium-browser-snapshots/Win_x64/LAST_CHANGE");
+            StreamReader reader = new StreamReader(stream);
+            string content = reader.ReadToEnd();
+            return content;
+        }
+
+
+        private void DownloadBuild()
+        {
+            string latestVer = CheckVer();
+            string downloadURL = "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Win_x64/" + latestVer + "/mini_installer.exe";
+            WebClient Client = new WebClient();
+            Client.DownloadFile(downloadURL, @"C:\TEMP\Chromium\mini_installer.exe");
+        }
+
+        public void UpdateBuild()
+        {
+            DownloadBuild();
+            Process.Start(@"C:\TEMP\Chromium\mini_installer.exe");
+        } 
+    }
+ }
